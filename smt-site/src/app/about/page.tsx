404542d9d9be25916a6f050bd3c23811f1acee2b
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-09-24 16:39:38 +0700

    Use NEXT.JS useRouter

--- smt-site/src/app/about/page.tsx
+++ smt-site/src/app/about/page.tsx
@@ -3,13 +3,13 @@
 import { useRouter } from "next/navigation";
 import { Button } from "@nextui-org/button";
 
-export default function Home() {
+export default function About() {
   const router = useRouter();
   return (
     <div className="container">
-      <h1>Hatyaiwit M.4/5</h1>
+      <h1>About M.4/5</h1>
       <h2>Powered by NEXT.JS with NEXTUI</h2>
-      <Button onClick={() => router.push("/about")}>About</Button>
+      <Button onClick={() => router.push("/")}>Home</Button>
     </div>
   );
 }
