7dc0e631cf7b5446acf458b548cf82deb00ee371
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-09-24 17:06:50 +0700

    Add custom 404 page

---
+++ smt-site/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "@nextui-org/button";
+
+export default function NotFound() {
+  const router = useRouter();
+  return (
+    <div className="container">
+      <h1>404 - Not found</h1>
+      <h2>Powered by NEXT.JS with NEXTUI</h2>
+      <Button onClick={() => router.push("/")}>Home</Button>
+    </div>
+  );
+}
