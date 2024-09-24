31355465d67031b2612de45ba28a8c7b3545fb41
kunzaka001 <137798282+kunzaka001@users.noreply.github.com>
2024-09-24 20:19:28 +0700

    add togglenav to pages

--- smt-site/src/app/about/page.tsx
+++ smt-site/src/app/about/page.tsx
@@ -1,17 +1,25 @@
 "use client";
-
 import { useRouter } from "next/navigation";
 import SideNavbar from "../components/navbar";
+import Tognav from "../components/togglenav"
 import { Button } from "@nextui-org/button";
+import { useState } from "react";
 
 export default function About() {
   const router = useRouter();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+      setIsVisible((prev) => !prev); 
+  };
+
   return (
     <div className="container">
-      <SideNavbar />
+      { isVisible && <SideNavbar /> }
       <h1>About M.4/5</h1>
       <h2>Powered by NEXT.JS with NEXTUI</h2>
       <Button onClick={() => router.push("/")}>Home</Button>
+      <Tognav isVisible={isVisible} toggleVisibility={toggleVisibility} />
     </div>
   );
 }
