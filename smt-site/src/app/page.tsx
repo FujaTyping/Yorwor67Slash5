29cbed3853eadbeced6d0e7b3cdc8170458c55b9
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-09-25 19:56:52 +0700

    [NOT TEST] Firebase SDK

--- smt-site/src/app/page.tsx
+++ smt-site/src/app/page.tsx
@@ -1,8 +1,20 @@
-export default function Home() {
+import { useState } from 'react';
+import { doc, getDoc } from "firebase/firestore";
+import db from "../../firebase-config.js";
+
+export default async function Home() {
+  const [data,setData] = useState(null)
+  const docRef = doc(db, "homework", "1");
+  const docSnap = await getDoc(docRef);
+  setData(docSnap.data())
   return (
     <div className="container">
       <h1>Hatyaiwit M.4/5</h1>
       <h2>Powered by NEXT.JS with Flowbite</h2>
+      <br />
+      <div>
+        DATA : {data}
+      </div>
     </div>
   );
 }
