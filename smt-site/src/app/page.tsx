9ef8a25cebabe171b254260d0363b53c66279ab1
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-09-25 17:27:15 -0700

    Implement API

--- smt-site/src/app/page.tsx
+++ smt-site/src/app/page.tsx
@@ -1,8 +1,14 @@
-export default function Home() {
+import axios from 'axios';
+import Marquee from "react-fast-marquee";
+
+export default async function Home() {
+  const response = await axios.get('https://api.smt.siraphop.me/announcement');
+  const announcment = response.data.Text;
   return (
     <div className="container">
       <h1>Hatyaiwit M.4/5</h1>
       <h2>Powered by NEXT.JS with Flowbite</h2>
+      <h2 className='gap-3' style={{ display: 'inline-flex' }}>ประกาศ:<Marquee style={{maxWidth: '300px'}} pauseOnHover={true}>{announcment}</Marquee></h2>
     </div>
   );
 }
