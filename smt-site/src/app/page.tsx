7f1c05714f968d55769812992bf42388b89e1e9c
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-09-26 02:52:46 -0700

    Update page to client side

--- smt-site/src/app/page.tsx
+++ smt-site/src/app/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+
+import { useState, useEffect } from 'react'
 import axios from 'axios';
 import Marquee from "react-fast-marquee";
 
-export default async function Home() {
-  const response = await axios.get('https://api.smt.siraphop.me/announcement');
-  const announcment = response.data.Text;
+export default function Home() {
+  const [data, setData] = useState('Fetching')
+
+  useEffect(() => {
+    axios
+      .get(`https://api.smt.siraphop.me/announcement`)
+      .then((response) => {
+        setData(response.data.Text)
+      })
+      .catch((error) => {
+        setData(`${error}`)
+      });
+  }, [])
+
   return (
     <div className="container">
       <h1>Hatyaiwit M.4/5</h1>
       <h2>Powered by NEXT.JS with Flowbite</h2>
-      <h2 className='gap-3' style={{ display: 'inline-flex' }}>ประกาศ:<Marquee style={{maxWidth: '300px'}} pauseOnHover={true}>{announcment}</Marquee></h2>
+      <h2 className='gap-3' style={{ display: 'inline-flex' }}>ประกาศ:<Marquee style={{ maxWidth: '300px' }} pauseOnHover={true}>{data}</Marquee></h2>
     </div>
   );
 }
