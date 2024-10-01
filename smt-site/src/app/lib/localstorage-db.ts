bcf55faf0643f90994a0eae27eb47f5ec393b5df
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-10-02 01:50:22 +0700

    Add idb

--- smt-site/src/app/lib/localstorage-db.ts
+++ smt-site/src/app/lib/localstorage-db.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { openDB } from 'idb';
 
 const useLocalStorge = (IsAdminpage: boolean) => {
   const [email, setEmail] = useState("");
@@ -11,25 +12,19 @@ const useLocalStorge = (IsAdminpage: boolean) => {
   const router = useRouter();
 
   useEffect(() => {
-    const request = indexedDB.open("firebaseLocalStorageDb", 1);
-
-    request.onsuccess = (event) => {
-      const db = (event.target as IDBOpenDBRequest).result;
-      const transaction = db.transaction(["firebaseLocalStorage"], "readonly");
-      const objectStore = transaction.objectStore("firebaseLocalStorage");
-
-      const getAllRequest = objectStore.getAll();
-
-      getAllRequest.onsuccess = () => {
-        const data = getAllRequest.result;
-        const user = data[0]?.value;
-
-        if (!user) {
-          return router.push("/");
+    async function getData() {
+      const db = await openDB("firebaseLocalStorageDb");
+      const store = db.transaction("firebaseLocalStorage").objectStore("firebaseLocalStorage");
+      const value = await store.getAll();
+      if (value.length == 1) {
+        const user = value[0]?.value;
+        if (user) {
+          setPhotourl(user.photoURL);
         }
-
-        setPhotourl(user.photoURL);
         if (IsAdminpage) {
+          if (!user) {
+            return router.push("/");
+          }
           setEmail(user.email);
           setUsername(user.displayName);
           if (!user.email.includes("@hatyaiwit.ac.th")) {
@@ -38,16 +33,26 @@ const useLocalStorge = (IsAdminpage: boolean) => {
             setShowAlert(false);
           }
         }
-      };
-
-      getAllRequest.onerror = () => {
-        console.error("Error accessing firebaseLocalStorage");
-      };
-    };
-
-    request.onerror = (event) => {
-      console.error((event.target as IDBOpenDBRequest).error);
-    };
+      } else {
+        const user = value[1]?.value;
+        if (user) {
+          setPhotourl(user.photoURL);
+        }
+        if (IsAdminpage) {
+          if (!user) {
+            return router.push("/");
+          }
+          setEmail(user.email);
+          setUsername(user.displayName);
+          if (!user.email.includes("@hatyaiwit.ac.th")) {
+            setShowAlert(true);
+          } else {
+            setShowAlert(false);
+          }
+        }
+      }
+    }
+    getData();
   }, []);
 
   return { email, username, photourl, showAlert };
