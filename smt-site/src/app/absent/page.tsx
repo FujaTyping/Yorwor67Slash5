eb81c5dd3e3dd3fcc5b0657c8c6227842d5927a3
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-10-07 13:22:25 +0700

    Add charts to absent data

--- smt-site/src/app/absent/page.tsx
+++ smt-site/src/app/absent/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Table, Pagination } from "flowbite-react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { CgGirl, CgBoy } from "react-icons/cg";
 import { PiStudentFill } from "react-icons/pi";
 import { FaRunning } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
 
 interface Absent {
   Date: string;
   Number: string;
   All: string;
+  Count: string;
+}
+
+interface Charts {
+  name: string;
+  Count: string;
 }
 
 interface Staticdata {
@@ -27,6 +35,13 @@ export default function Absent() {
       Date: "กำลังดึงข้อมูล",
       Number: "กำลังดึงข้อมูล",
       All: "กำลังดึงข้อมูล",
+      Count: "0",
+    },
+  ]);
+  const [Chartsdata, setChartsData] = useState<Charts[]>([
+    {
+      name: "กำลังดึงข้อมูล",
+      Count: "กำลังดึงข้อมูล",
     },
   ]);
   const [Staticdata, setStaticdata] = useState<Staticdata>({
@@ -36,9 +51,16 @@ export default function Absent() {
     Absent: "0",
     Date: "กำลังดึงข้อมูล",
   });
+  const [showDetilsData, setshowDetilsData] = useState(false)
   const [title] = useState("Hatyaiwit - เช็คชื่อ");
   const [currentPage, setCurrentPage] = useState(1);
+  const DatadetailsRef = useRef<null | HTMLDivElement>(null);
   const itemsPerPage = 20;
+
+  const handleShowClick = () => {
+    setshowDetilsData(true);
+    DatadetailsRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   useEffect(() => {
     axios
@@ -46,6 +68,11 @@ export default function Absent() {
       .then((response) => {
         setData(response.data.Absent);
         setStaticdata(response.data.Static);
+        const chartData = response.data.Absent.slice(0, 7).reverse().map((item: Absent) => ({
+          name: `${item.Date}`,
+          Count: parseInt(item.Count),
+        }));
+        setChartsData(chartData);
       })
       .catch((error) => {
         setData([
@@ -53,6 +80,7 @@ export default function Absent() {
             Date: "ไม่สามารถ",
             Number: "ดึงข้อมูล",
             All: `${error}`,
+            Count: "0",
           },
         ]);
         setStaticdata({
@@ -129,47 +157,69 @@ export default function Absent() {
       </div>
       <div className="container">
         <h1 style={{ marginBottom: "15px" }} className="border-b">
-          📝 เช็คชื่อ - Absent
+          📊 สถิตินักเรียนรายสัปดาห์ - Chart
         </h1>
-        <div style={{ marginTop: "20px" }} className="overflow-x-auto">
-          <Table hoverable>
-            <Table.Head>
-              <Table.HeadCell>วันที่</Table.HeadCell>
-              <Table.HeadCell>เลขที่ขาด</Table.HeadCell>
-              <Table.HeadCell>สรุปสถิติ</Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y">
-              {currentData.map((Absent, index) => (
-                <Table.Row
-                  key={index}
-                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                >
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    {Absent.Date}
-                  </Table.Cell>
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900">
-                    {Absent.Number}
-                  </Table.Cell>
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900">
-                    {Absent.All}
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-          <div style={{ flexDirection: 'column', alignItems: 'center', marginTop: '17px' }} className="flex justify-center">
-            <p>แสดง {startItem}-{endItem} รายการ ทั้งหมด {data.length} รายการ</p>
-            <Pagination
-              style={{ marginTop: '-20px' }}
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={handlePageChange}
-              previousLabel="ก่อนหน้า"
-              nextLabel="ถัดไป"
-            />
-          </div>
-        </div>
+        <h2 style={{ fontSize: "18px" }}>
+          สรุปจำนวนการขาด / ลา ของนักเรียนตลอด 7 วันที่ผ่านมา<br /><span onClick={handleShowClick} className="flex" style={{ cursor: 'pointer', alignItems: 'center' }}><IoEyeSharp style={{ marginRight: '6px' }} /> ดูประวัติการเช็คชื่อ</span>
+        </h2>
+        <ResponsiveContainer style={{ marginTop: '25px' }} width="100%" height={300}>
+          <LineChart data={Chartsdata} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="Count" name="นักเรียนที่ไม่มา" stroke="#ff1616" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
+      {showDetilsData && (
+        <>
+          <div id="StuDetils" ref={DatadetailsRef} className="container animate__animated animate__fadeInUp">
+            <h1 style={{ marginBottom: "15px" }} className="border-b">
+              📝 เช็คชื่อ - Absent
+            </h1>
+            <div style={{ marginTop: "20px" }} className="overflow-x-auto">
+              <Table hoverable>
+                <Table.Head>
+                  <Table.HeadCell>วันที่</Table.HeadCell>
+                  <Table.HeadCell>เลขที่ขาด</Table.HeadCell>
+                  <Table.HeadCell>สรุปสถิติ</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                  {currentData.map((Absent, index) => (
+                    <Table.Row
+                      key={index}
+                      className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                    >
+                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                        {Absent.Date}
+                      </Table.Cell>
+                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900">
+                        {Absent.Number}
+                      </Table.Cell>
+                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900">
+                        {Absent.All}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+              <div style={{ flexDirection: 'column', alignItems: 'center', marginTop: '17px' }} className="flex justify-center">
+                <p>แสดง {startItem}-{endItem} รายการ ทั้งหมด {data.length} รายการ</p>
+                <Pagination
+                  style={{ marginTop: '-20px' }}
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                  previousLabel="ก่อนหน้า"
+                  nextLabel="ถัดไป"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
