6dc2383737694a54a2f0c736f437522179d495e4
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-11-19 15:15:19 +0700

    Update page.tsx

--- smt-site/src/app/tool/teacher/wheel/page.tsx
+++ smt-site/src/app/tool/teacher/wheel/page.tsx
@@ -92,7 +92,7 @@ export default function Wheel() {
                 <section className="text-gray-600 body-font">
                     <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                            <img className="object-cover object-center rounded" alt={student ? student.name : "Student"}
+                            <img style={{ width: "350px" }} className="object-cover object-center rounded" alt={student ? student.name : "Student"}
                                 src={student ? student.avatar : "https://media.istockphoto.com/id/1410224257/vector/group-of-students-stand-together-flat-vector-illustration-young-girls-and-boys-holding-books.jpg?s=612x612&w=0&k=20&c=ih5WHSOcCRnySxpRxc259pWq8v0RacFjsaGheDTAiWI="} />
                         </div>
                         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
