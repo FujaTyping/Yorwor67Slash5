db5b9757abf5a49ff1d62fc75028f1b147e00aea
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-11-21 12:59:40 +0700

    Add Hell mode

--- smt-site/src/app/tool/teacher/wheel/page.tsx
+++ smt-site/src/app/tool/teacher/wheel/page.tsx
@@ -13,6 +13,7 @@ interface Student {
     nickname: string;
     number: string;
     avatar: string;
+    savatar?: string;
 }
 
 export default function Wheel() {
@@ -22,6 +23,8 @@ export default function Wheel() {
     const [isAnimating, setIsAnimating] = useState<boolean>(false);
     const [tickPlay] = useSound("/assets/Sound/Tick.mp3");
     const [conPlay] = useSound("/assets/Sound/Confetti.mp3", { volume: 0.5 });
+    const [titlemessage, setTitleMessage] = useState("😲 สุ่มชื่อนักเรียน - Wheel of Names");
+    const [toggleHellmode, settoggleHellmode] = useState(false);
     const [studentData, SetStudentData] = useState<Student[]>([
         {
             name: "กำลังดึงข้อมูล",
@@ -32,14 +35,48 @@ export default function Wheel() {
     ]);
     const [studentDis, SetStudentDis] = useState("https://media.istockphoto.com/id/1410224257/vector/group-of-students-stand-together-flat-vector-illustration-young-girls-and-boys-holding-books.jpg?s=612x612&w=0&k=20&c=ih5WHSOcCRnySxpRxc259pWq8v0RacFjsaGheDTAiWI=");
 
+    const shakeAnimation = {
+        initial: { scale: 1 },
+        animate: {
+            scale: [1, 1.1, 0.9, 1.2, 0.8, 1],
+            transition: {
+                duration: 0.6,
+                ease: "easeInOut",
+            },
+        },
+    };
+
+    const toggleMessage = () => {
+        let newMessage;
+        if (titlemessage === "😲 สุ่มชื่อนักเรียน - Wheel of Names") {
+            newMessage = "😈 สุ่มชื่อนักเรียน - Wheel of Hell";
+            SetStudentDis("https://cdn.discordapp.com/attachments/1249685919267684406/1309129234182180917/1732191118690.jpg?ex=6740751b&is=673f239b&hm=209943a144f499c9cf3c367637c184b34a8d9f5487ae1545f5378a5ddd6a703a&");
+            settoggleHellmode(true);
+        } else {
+            newMessage = "😲 สุ่มชื่อนักเรียน - Wheel of Names";
+            SetStudentDis("https://media.istockphoto.com/id/1410224257/vector/group-of-students-stand-together-flat-vector-illustration-young-girls-and-boys-holding-books.jpg?s=612x612&w=0&k=20&c=ih5WHSOcCRnySxpRxc259pWq8v0RacFjsaGheDTAiWI=");
+            settoggleHellmode(false);
+        }
+        setTitleMessage(newMessage);
+    };
+
     const randomStudent = () => {
         setIsAnimating(true);
         setConfitiC(0);
-        SetStudentDis("https://media.istockphoto.com/id/1410224257/vector/group-of-students-stand-together-flat-vector-illustration-young-girls-and-boys-holding-books.jpg?s=612x612&w=0&k=20&c=ih5WHSOcCRnySxpRxc259pWq8v0RacFjsaGheDTAiWI=");
+        if (toggleHellmode) {
+            SetStudentDis("https://cdn.discordapp.com/attachments/1249685919267684406/1309129234182180917/1732191118690.jpg?ex=6740751b&is=673f239b&hm=209943a144f499c9cf3c367637c184b34a8d9f5487ae1545f5378a5ddd6a703a&");
+        } else {
+            SetStudentDis("https://media.istockphoto.com/id/1410224257/vector/group-of-students-stand-together-flat-vector-illustration-young-girls-and-boys-holding-books.jpg?s=612x612&w=0&k=20&c=ih5WHSOcCRnySxpRxc259pWq8v0RacFjsaGheDTAiWI=");
+        }
 
         let count = 0;
-        const totalFlashes = 25;
+        let totalFlashes = 25;
         let delay = 100;
+
+        if (toggleHellmode) {
+            totalFlashes = 45;
+            delay = 50;
+        }
 
         const selectRandomStudent = () => {
             const randomIndex = Math.floor(Math.random() * studentData.length);
@@ -51,7 +88,11 @@ export default function Wheel() {
                     const finalIndex = Math.floor(Math.random() * studentData.length);
                     const finalStudent = studentData[finalIndex];
                     setStudent(finalStudent);
-                    SetStudentDis(finalStudent.avatar);
+                    if (toggleHellmode) {
+                        SetStudentDis(finalStudent.savatar ?? finalStudent.avatar);
+                    } else {
+                        SetStudentDis(finalStudent.avatar);
+                    }
 
                     SetStudentData((prevData) => prevData.filter((_, index) => index !== finalIndex));
 
@@ -63,7 +104,11 @@ export default function Wheel() {
                 return;
             }
 
-            delay += 20;
+            if (toggleHellmode) {
+                delay += 10;
+            } else {
+                delay += 20;
+            }
             tickPlay();
             setTimeout(selectRandomStudent, delay);
         };
@@ -95,13 +140,21 @@ export default function Wheel() {
             <meta property="og:title" content={title} />
             <Confetti mode="fall" shapeSize={15} particleCount={confitiC} />
             <div className="container">
-                <h1 style={{ marginBottom: "15px" }} className="border-b">
-                    😲 สุ่มชื่อนักเรียน - Wheel of Names
+                <h1
+                    style={{ marginBottom: "15px", cursor: "pointer" }}
+                    className="border-b"
+                    onClick={toggleMessage}
+                >
+                    {titlemessage}
                 </h1>
                 <h2 style={{ fontSize: "18px" }}>
-                    สุ่มชื่อนักเรียนทั้งหมด 1 คน จาก {studentData.length + 1} คนของห้อง ม.4/5
+                    สุ่มชื่อนักเรียนทั้งหมด 1 คน จาก {studentData.length} คนของห้อง ม.4/5
                 </h2>
-                <section className="text-gray-600 body-font">
+                <motion.section
+                    className="text-gray-600 body-font"
+                    key={toggleHellmode ? "hellMode" : "normalMode"}
+                    {...(toggleHellmode ? shakeAnimation : {})}
+                >
                     <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                         <motion.div
                             className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0"
@@ -148,7 +201,7 @@ export default function Wheel() {
                             </div>
                         </motion.div>
                     </div>
-                </section>
+                </motion.section>
             </div>
         </>
     )
