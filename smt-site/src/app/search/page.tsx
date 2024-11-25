b0aaff24c7d073ab052d986a1ac39ead106e37bb
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-11-25 12:07:16 +0700

    some changes

--- smt-site/src/app/search/page.tsx
+++ smt-site/src/app/search/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import SMT from "../assets/SMT.png"
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Search() {
     const [title] = useState("Hatyaiwit - ค้นหา");
@@ -26,6 +27,10 @@ export default function Search() {
         }
     };
 
+    function searchG() {
+        window.location.href = `https://www.google.com/search?q=${q}`
+    }
+
     return (
         <>
             <title>{title}</title>
@@ -39,6 +44,25 @@ export default function Search() {
                         <img className="h-6 w-7" src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" alt="Google logo" />
                     </div>
                     <input onKeyDown={handleKeyDown} onChange={(q) => { setQ(q.target.value) }} placeholder="ค้นหาด้วย Google" type="text" className="w-full bg-transparent rounded-full py-[14px] pl-4 outline-none" />
+                    <FaArrowRight onClick={searchG} style={{ color: 'gray', cursor: 'pointer' }} className="h-4 w-4 mr-5" />
+                </div>
+                <div className="flex mt-10 items-center gap-10">
+                    <div onClick={() => { window.location.href = `https://classroom.google.com/` }} style={{ cursor: 'pointer' }} className="flex flex-col items-center gap-2 hover:drop-shadow-2xl">
+                        <img className="w-14 h-12" src="https://th.bing.com/th/id/R.d0c3f5e1f3abd729190c29cd67982b17?rik=rBXAcV9kfKXVeA&pid=ImgRaw&r=0" alt="Classroom Logo" />
+                        <h1 style={{ fontSize: '13px' }}>Classroom</h1>
+                    </div>
+                    <div onClick={() => { window.location.href = `https://youtube.com/` }} style={{ cursor: 'pointer' }} className="flex flex-col items-center gap-2 hover:drop-shadow-2xl">
+                        <img className="w-12 h-12" src="https://lh3.googleusercontent.com/3_OFn2skqHXk-UQ-9RUdNrDl_HQJrMCxks5teQcUrF_bOSeDG1hD8j83FeD31W8hASZCvubzsGfumuJq8kvvSAq03wY87RZ7Otx_DF4" alt="Youtube Logo" />
+                        <h1 style={{ fontSize: '13px' }}>Youtube</h1>
+                    </div>
+                    <div onClick={() => { window.location.href = `https://mail.google.com/` }} style={{ cursor: 'pointer' }} className="flex flex-col items-center gap-2 hover:drop-shadow-2xl">
+                        <img className="w-12 h-12" src="https://lh3.googleusercontent.com/0rpHlrX8IG77awQMuUZpQ0zGWT7HRYtpncsuRnFo6V3c8Lh2hPjXnEuhDDd-OsLz1vua4ld2rlUYFAaBYk-rZCODmi2eJlwUEVsZgg" alt="Mail Logo" />
+                        <h1 style={{ fontSize: '13px' }}>Mail</h1>
+                    </div>
+                    <div onClick={() => { window.location.href = `https://smt.siraphop.me/` }} style={{ cursor: 'pointer' }} className="flex flex-col items-center gap-2 hover:drop-shadow-2xl">
+                        <img className="w-10 h-12" src="https://upload.wikimedia.org/wikipedia/commons/6/6f/%E0%B8%95%E0%B8%A3%E0%B8%B5%E0%B8%88%E0%B8%B1%E0%B8%81%E0%B8%A3.png" alt="Yw Logo" />
+                        <h1 style={{ fontSize: '13px' }}>ม.4/5</h1>
+                    </div>
                 </div>
             </div>
         </>
