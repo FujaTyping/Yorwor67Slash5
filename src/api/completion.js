627703230e97657eb2a4f4ec59f23e4654e70dd1
kunzaka001 <kunnoppakun007@gmail.com>
2024-12-27 20:03:30 +0700

    Create completion.js

---
+++ src/api/completion.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { doc, getDocs, setDoc } = require('firebase/firestore');
+const { Authenticate } = require('../utils/authenticate');
+
+let ComRealData = {
+    Completion: [],
+};
+
+module.exports = (db) =>  {
+    const router = express.Router();
+
+    router.get("/", async (req, res) => {
+      if (Date.now() - ComlastFetchTime > fetchInterval) {
+        const querySnapshot = await getDocs(query(collection(db, "Completion"), orderBy("timestamp", "desc")));
+        ComRealData.Completion = [];
+        querySnapshot.forEach((doc) => {
+          ComRealData.Completion.push(doc.data());
+        });
+        ComlastFetchTime = Date.now();
+      }
+      res.send(ComRealData);
+    });
+
+    router.post("/", Authenticate, async (req, res) => {
+      const Title = req.body.title;
+      const Decs = req.body.decs;
+      const Url = req.body.url;
+      const Time = req.body.time;
+      if (!Title || !Decs || !Url || !Time) {
+        res.status(400).send("กรุณากรอกข้อมูลให้ครบถ้วน");
+      } else {
+        const UID = generateID();
+        await setDoc(doc(db, "Completion", `${UID}`), {
+          Title: `${Title}`,
+          Decs: `${Decs}`,
+          Url: `${Url}`,
+          Time: `${Time}`,
+          timestamp: serverTimestamp(),
+        });
+        res.send(`เพิ่มข้อมูลด้วยไอดี ${UID} เรียบร้อยแล้ว`);
+      }
+    });
+
+    return {
+        baseRoute: '/completion',
+        router,
+    };
+}
