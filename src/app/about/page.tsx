71c90405293879dc4c58221ac8dc0e32f9f7ceb3
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-01-01 13:44:55 +0700

    Some changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -43,14 +43,14 @@ export default function About() {
         </div>
       </div>
       <div className="container">
-        <div className="-mt-3 md:-mt-0">
+        <div className="-mt-3 md:-mt-0 md:mb-2">
           <div className="mb-5 flex items-center justify-between gap-8 md:mb-6">
             <div className="flex flex-col md:flex-row items-center gap-3 md:gap-10">
-              <span className="relative whitespace-nowrap ml-4">
+              <span className="relative whitespace-nowrap">
                 <svg aria-hidden="true" viewBox="0 0 418 42" className="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-500 dark:fill-orange-300/60" preserveAspectRatio="none">
                   <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.780 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.540-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.810 23.239-7.825 27.934-10.149 28.304-14.005 .417-4.348-3.529-6-16.878-7.066Z"></path>
                 </svg>
-                <h2 className="text-2xl font-bold lg:text-3xl dark:text-white">SMT</h2>
+                <h2 className="text-2xl font-bold lg:text-3xl">SMT</h2>
               </span>
               <p>
                 ย่อมาจาก Science Math Technology หลักสูตรที่เน้นวิทยาศาสตร์ คณิตศาสตร์ และเทคโนโลยี
@@ -94,15 +94,6 @@ export default function About() {
             </Link>
           </div>
         </div>
-      </div>
-      <div style={{ marginTop: "-2.5rem" }} className="container font-bold">
-        <Marquee gradient={true} gradientColor="white" gradientWidth={25}>
-          <p>เรื่องคิดเลขอ่ะเกรดหนึ่ง เรื่องคิดถึงอ่ะเกรดสี่</p><FaCircle />
-          <p>การบ้านเท่าภูเขา ความโง่เขลาเท่าจักรวาล</p><FaCircle />
-          <p>งานเลี้ยงย่อมมีวันเลิกรา อาจาร์ยเข้าช้า ไม่มีวันเลิกเร็ว</p><FaCircle />
-          <p>คำที่ครูบอกออกสอบ มักจะไม่เจอในข้อสอบเสมอ</p><FaCircle />
-          <p>เรื่องเรียนเป็นเรื่องตลก สอบตกเป็นเรื่องธรรมดา</p><FaCircle />
-        </Marquee>
       </div>
     </>
   );
