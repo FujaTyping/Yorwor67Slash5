b504daad853858d8c493b258cb602687a30a9d01
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-04-14 01:57:35 +0700

    Add cooldown

--- src/api/github.js
+++ src/api/github.js
@@ -1,31 +1,39 @@
 const express = require('express');
 const axios = require('axios');
 
+let RealData = { data: [] };
+let lastFetchTime = 0;
+const fetchInterval = 5 * 60 * 1000;
+
 module.exports = (db) => {
     const router = express.Router();
 
     router.get('/version', async (req, res) => {
-        try {
-            const response = await axios.get("https://api.github.com/repos/FujaTyping/Yorwor67Slash5/commits?sha=dev&per_page=1&page=1");
+        if (Date.now() - lastFetchTime > fetchInterval) {
+            try {
+                const response = await axios.get("https://api.github.com/repos/FujaTyping/Yorwor67Slash5/commits?sha=dev&per_page=1&page=1");
 
-            const linkHeader = response.headers['link'];
+                const linkHeader = response.headers['link'];
 
-            if (!linkHeader) {
-                return res.status(400).send('No link header found');
+                if (!linkHeader) {
+                    return res.status(400).send('No link header found');
+                }
+
+                const match = linkHeader.match(/page=(\d+)>; rel="last"/);
+
+                if (match) {
+                    const version = match[1];
+                    RealData.data.push({ version });
+                    lastFetchTime = Date.now();
+                } else {
+                    return res.status(400).send('No last page found in link header');
+                }
+            } catch (error) {
+                console.error(error);
+                return res.status(500).send('Error fetching from GitHub');
             }
-
-            const match = linkHeader.match(/page=(\d+)>; rel="last"/);
-
-            if (match) {
-                const version = match[1];
-                return res.send({ version });
-            } else {
-                return res.status(400).send('No last page found in link header');
-            }
-        } catch (error) {
-            console.error(error);
-            return res.status(500).send('Error fetching from GitHub');
         }
+        res.send(RealData);
     });
 
     return {
