641f3c75af7a833e27503091215fd59ec5d416c0
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-04-14 20:54:23 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,8 +4,11 @@ import Backprev from './components/backprev'
 function notfound() {
     return (
         <>
-            <div className="px-6 w-full flex gap-3 flex-col items-center justify-center">
-                <h1 className="font-bold text-xl">ดูเหมือนคุณกำลังหลงทางสินะ</h1>
+            <div className="px-6 py-4 w-full flex gap-3 flex-col items-center justify-center">
+                <div className='text-center'>
+                    <h1 className="font-bold text-xl mb-1">หลงทางเก่งเหมือนกันนะ</h1>
+                    <p className='text-xs'>หน้านี้ไม่มีแล้วจ้าา เหมือนวิญญาณหลังสอบเสร็จที่ปลิวหายไป ไม่เหลือแม้แต่เศษศักดิ์ศรี กดกลับไปซะ ก่อนสมองจะหลงตามมาด้วย</p>
+                </div>
                 <Backprev />
             </div>
         </>
