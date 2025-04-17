c5c94f20440ec344153859f7eff46c5f07fa6802
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-04-18 01:35:15 +0700

    Add edit to homework

--- src/api/assignment.js
+++ src/api/assignment.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getDocs, query, collection, orderBy, setDoc, doc, serverTimestamp } = require('firebase/firestore');
+const { getDocs, query, collection, orderBy, setDoc, doc, serverTimestamp, getDoc, updateDoc } = require('firebase/firestore');
 const { Authenticate } = require('../utils/authenticate');
 const { generateID } = require('../lib/module');
 const pushNewHomework = require("../lib/lineOA/pushHomework");
@@ -32,6 +32,7 @@ module.exports = (db) => {
                 HRealData.Homework = [];
                 querySnapshot.forEach((doc) => {
                     const homework = doc.data();
+                    homework.id = doc.id;
                     const dueDate = thaiDateToJsDate(homework.Due);
                     homework.isDue = Date.now() > dueDate.getTime();
                     HRealData.Homework.push(homework);
@@ -73,11 +74,50 @@ module.exports = (db) => {
             });
 
             await pushNewHomework(SDate, Subject, Decs, DDate);
-            await notifyHomework(SDate, Subject, Decs, DDate);
+            await notifyHomework(db, SDate, Subject, Decs, DDate);
 
             res.send(`เพิ่มข้อมูลด้วยไอดี ${UID} เรียบร้อยแล้ว`);
         } catch (e) {
             res.status(500).send(`Error adding homework data: ${e.message}`);
+        }
+    });
+
+    router.patch('/', Authenticate(db), async (req, res) => {
+        const { id: ID, decs: Decs, due: Due, subj: Subject, time: Time } = req.body;
+
+        if (!Decs || !Due || !Subject || !Time || !ID) {
+            return res.status(400).send("กรุณากรอกข้อมูลให้ครบถ้วน");
+        }
+
+        const docRef = doc(db, "Homework", ID);
+        const ADoc = await getDoc(docRef);
+
+        if (!ADoc.exists()) {
+            return res.status(400).send("ไม่พบไอดีของข้อมูลนี้");
+        }
+
+        try {
+            const SDate = new Date(Time).toLocaleDateString('th-TH', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            })
+            const DDate = new Date(Due).toLocaleDateString('th-TH', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            })
+
+            await updateDoc(docRef, {
+                Decs: `${Decs}`,
+                Due: `${DDate}`,
+                Subject: `${Subject}`,
+                Time: `${SDate}`,
+                timestamp: serverTimestamp(),
+            });
+            res.send(`อัพเดทข้อมูลของไอดี ${ID} เรียบร้อยแล้ว`);
+        } catch (error) {
+            res.status(500).send(`Error editing homework data: ${error.message}`);
         }
     });
 
--- src/lib/dsgHook/notifyHomework.js
+++ src/lib/dsgHook/notifyHomework.js
@@ -1,26 +1,11 @@
 const axios = require("axios");
-const { initializeApp } = require("firebase/app");
 const {
     collection,
     getDocs,
-    getFirestore
-
 } = require("firebase/firestore");
 require("dotenv").config();
 
-const firebaseConfig = {
-    apiKey: process.env.ApiKey,
-    authDomain: process.env.AuthDomain,
-    projectId: process.env.ProjectId,
-    storageBucket: process.env.StorageBucket,
-    messagingSenderId: process.env.MessagingSenderId,
-    appId: process.env.AppId,
-    measurementId: process.env.MeasurementId,
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-async function notifyHomework(Time, Subject, Decs, Due) {
+async function notifyHomework(db, Time, Subject, Decs, Due) {
     const querySnapshot = await getDocs(collection(db, "DiscordWebhooks"));
     querySnapshot.forEach((doc) => {
         const Url = doc.data().WebhookUrl
