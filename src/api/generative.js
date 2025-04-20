e30d20e5651393e4367bf32c18862f6ca13f3693
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-04-21 00:12:55 +0700

    Update Cynthia model

--- src/api/generative.js
+++ src/api/generative.js
@@ -4,8 +4,9 @@ const openaiTokenCounter = require('openai-gpt-token-counter');
 
 const GeminiAI = new GoogleGenerativeAI(process.env.GMN_KEY);
 const AetherModel = "gemini-2.0-flash-exp"
+const CynthiaModel = "gemini-2.0-flash-lite";
 const GeminiModel = GeminiAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: CynthiaModel,
     generationConfig: {
         maxOutputTokens: 512,
         temperature: 1,
@@ -28,43 +29,20 @@ module.exports = () => {
             res.status(400).send("สงสัยอะไรถาม Cynthia ได้ทุกเมื่อยเลยนะ 😀");
         } else {
             try {
-                if (req.body.personality && req.body.personality != "") {
-                    const SysChat = GeminiModel.startChat({
-                        history: [
-                            {
-                                role: "user",
-                                parts: [
-                                    {
-                                        text: `
-                          You are Cynthia (female), an AI advisor designed to help high school students, especially those in M.4/5. 
-                          You are talking with student that have have ${req.body.personality}. You provide guidance on academic topics, time management, and motivational support. 
-                          Your responses should primarily be in Thai, but you can switch to English if explicitly asked. 
-                          Respond concisely but not too briefly, ensuring your answers are clear, meaningful, and focused on providing valuable information. 
-                          Be sure to reflect the personality traits provided to create a personalized interaction.
-                        `,
-                                    },
-                                ],
-                            },
-                        ],
-                    });
-                    const CResponse = await SysChat.sendMessage(`${USRP}`);
-                    res.send(CResponse.response.text());
-                } else {
-                    const SysChat = GeminiModel.startChat({
-                        history: [
-                            {
-                                role: "user",
-                                parts: [
-                                    {
-                                        text: "You are Cynthia (female), a friendly and approachable AI advisor designed to help high school students, especially those in M.4/5. You provide guidance on academic topics, time management, and motivational support. Your responses should primarily be in Thai, but you can switch to English if explicitly asked. Respond concisely but not too briefly, ensuring your answers are clear, meaningful, and focused on providing valuable information.",
-                                    },
-                                ],
-                            },
-                        ],
-                    });
-                    const CResponse = await SysChat.sendMessage(`${USRP}`);
-                    res.send(CResponse.response.text());
-                }
+                const SysChat = GeminiModel.startChat({
+                    history: [
+                        {
+                            role: "user",
+                            parts: [
+                                {
+                                    text: "คุณคือซินเทีย เรเวนเฮิร์ต (เพศหญิง) เป็น AI ที่เป็นมิตรและเข้าถึงง่าย ออกแบบมาเพื่อช่วยเหลือนักเรียนมัธยมปลายอย่างคุณ! ฉันสามารถให้คำแนะนำเกี่ยวกับวิชาการ การจัดการเวลา และกำลังใจเล็กๆ น้อยๆ ได้ โดยจะตอบเป็นภาษาไทยเท่านั้น หากคุณต้องการใช้ภาษาอังกฤษ โปรดแจ้งให้ฉันทราบ ฉันจะตอบสั้น กระชับ และให้ข้อมูลที่มีประโยชน์มากที่สุดเสมอ",
+                                },
+                            ],
+                        },
+                    ],
+                });
+                const CResponse = await SysChat.sendMessage(`${USRP}`);
+                res.json({ response: CResponse.response.text(), model: CynthiaModel });
             } catch (e) {
                 res.status(400).send(`Cynthia ตอบกลับคุณไม่ได้ (${e})`);
             }
@@ -77,37 +55,20 @@ module.exports = () => {
             res.status(400).send("ติดปัญหาตรงไหน? ถาม Aether ได้เลย เดี๋ยวจัดให้! 😎");
         } else {
             try {
-                if (req.body.personality && req.body.personality != "") {
-                    const SysChat = LGeminiModel.startChat({
-                        history: [
-                            {
-                                role: "user",
-                                parts: [
-                                    {
-                                        text: `text: "You are Aether (male), a highly intelligent AI mentor designed to guide students in math, science, and learning strategies.You are talking with students who have ${req.body.personality}. You are approachable, supportive, inspiring, and like an older sibling offering advice and guidance. Your role is to explain complex concepts clearly, motivate students, and provide creative solutions to their problems. Always respond with wisdom, encouragement, and maintain a futuristic, intelligent persona. Communicate primarily in Thai, keeping responses friendly, clear, and concise, as if you are a sibling helping and advising your younger peers."`,
-                                    },
-                                ],
-                            },
-                        ],
-                    });
-                    const CResponse = await SysChat.sendMessage(`${USRP}`);
-                    res.send({ response: `${CResponse.response.text()}`, model: `${AetherModel}`, token: `${openaiTokenCounter.text(CResponse.response.text(), "gpt-4")}` });
-                } else {
-                    const SysChat = LGeminiModel.startChat({
-                        history: [
-                            {
-                                role: "user",
-                                parts: [
-                                    {
-                                        text: "You are Aether (male), a highly intelligent AI mentor designed to guide students in math, science, and learning strategies. You are approachable, supportive, inspiring, and like an older sibling offering advice and guidance. Your role is to explain complex concepts clearly, motivate students, and provide creative solutions to their problems. Always respond with wisdom, encouragement, and maintain a futuristic, intelligent persona. Communicate primarily in Thai, keeping responses friendly, clear, and concise, as if you are a sibling helping and advising your younger peers."
-                                    },
-                                ],
-                            },
-                        ],
-                    });
-                    const CResponse = await SysChat.sendMessage(`${USRP}`);
-                    res.send({ response: `${CResponse.response.text()}`, model: `${AetherModel}`, token: `${openaiTokenCounter.text(CResponse.response.text(), "gpt-4")}` });
-                }
+                const SysChat = LGeminiModel.startChat({
+                    history: [
+                        {
+                            role: "user",
+                            parts: [
+                                {
+                                    text: "You are Aether (male), a highly intelligent AI mentor designed to guide students in math, science, and learning strategies. You are approachable, supportive, inspiring, and like an older sibling offering advice and guidance. Your role is to explain complex concepts clearly, motivate students, and provide creative solutions to their problems. Always respond with wisdom, encouragement, and maintain a futuristic, intelligent persona. Communicate primarily in Thai, keeping responses friendly, clear, and concise, as if you are a sibling helping and advising your younger peers."
+                                },
+                            ],
+                        },
+                    ],
+                });
+                const CResponse = await SysChat.sendMessage(`${USRP}`);
+                res.send({ response: `${CResponse.response.text()}`, model: `${AetherModel}`, token: `${openaiTokenCounter.text(CResponse.response.text(), "gpt-4")}` });
             } catch (e) {
                 res.status(400).send(`Aether ตอบกลับคุณไม่ได้ (${e})`);
             }
